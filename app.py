a5c059d35331cd4eb02f4c22f82395118b8c6b4c
eyal-sofer <62301163+eyal-sofer@users.noreply.github.com>
2021-04-23 14:12:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 """Main application file"""
-2	from flask import Flask
-3	app = Flask(__name__)
-4	
-5	@app.route('/<random_string>')
-6	def returnBackwardsString(random_string):
-7	    """Reverse and return the provided URI"""
-8	    return "".join(reversed(random_string))
-9	
-10	if __name__ == '__main__':
-11	    app.run(host='0.0.0.0', port=8080)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/<random_string>')
+def returnBackwardsString(random_string):
+    """Reverse and return the provided URI"""
+    return "".join(reversed(random_string))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
