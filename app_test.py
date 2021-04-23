dfcc26a8f45c6d72bb697d2c645dd118bccc449d
eyal-sofer <62301163+eyal-sofer@users.noreply.github.com>
2021-04-23 14:12:54 +0300

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,15 +1,15 @@
-"""Unit testing file for app.py"""
-2	from app import returnBackwardsString
-3	import unittest
-4	
-5	class TestApp(unittest.TestCase):
-6	    """Unit tests defined for app.py"""
-7	
-8	    def test_return_backwards_string(self):
-9	        """Test return backwards simple string"""
-10	        random_string = "This is my test string"
-11	        random_string_reversed = "gnirts tset ym si sihT"
-12	        self.assertEqual(random_string_reversed, returnBackwardsString(random_string))
-13	
-14	if __name__ == "__main__":
-15	    unittest.main()
+"""Unit test file for app.py"""
+from app import returnBackwardsString
+import unittest
+
+class TestApp(unittest.TestCase):
+    """Unit tests defined for app.py"""
+
+    def test_return_backwards_string(self):
+        """Test return backwards simple string"""
+        random_string = "This is my test string"
+        random_string_reversed = "gnirts tset ym si sihT"
+        self.assertEqual(random_string_reversed, returnBackwardsString(random_string))
+
+if __name__ == "__main__":
+    unittest.main()
